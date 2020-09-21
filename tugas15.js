7c36b1cce2dfb02a460e2427c51afba374c9b75a
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-21 10:12:50 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var AB = 8
+var BC = 6
+
+var a = Math.pow(AB,2)
+var b = Math.pow(BC,2)
+
+var AC = Math.sqrt(a+b)
+
+console.log("Nilai AB : ",AB)	
+console.log("Nilai BC : ",BC)
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah ",AC," cm")
